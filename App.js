caff8ffebe13374cbd5a68ca6725b8586c32e122
Guest <guest-eaylnm@h26817.sgb.skir.kr>
2021-01-21 23:09:59 +0900

    textbook

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import{
-    Wave
-} from './Wave.js';
+    waveGroup
+} from './waveGroup.js';
 
 class App{
+
     constructor(){
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
 
-        this.wave = new Wave();
+        this.waveGroup = new waveGroup();
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
+
         requestAnimationFrame(this.animate.bind(this));
     }
 
@@ -23,13 +25,13 @@ class App{
         this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2,2);
 
-        this.wave.resize(this.stageWidth,this.stageHeight);
+        this.waveGroup.resize(this.stageWidth,this.stageHeight);
     }
 
     animate(t){
         this.ctx.clearRect(0,0,this.stageWidth,this.stageHeight);
 
-        this.wave.draw(this.ctx);
+        this.waveGroup.draw(this.ctx);
 
         requestAnimationFrame(this.animate.bind(this));
     }
--- Wave.js
+++ Wave.js
@@ -1,10 +1,13 @@
 import{
     Point
-} from './Point.js';
+}from './Point.js';
 
 export class Wave {
-    constructor(){
-
+    constructor(index, totalPoints, color){
+        this.index = index;
+        this.totalPoints = totalPoints;
+        this.color = color;
+        this.points = [];
     }
 
     resize(stageWidth, stageHeight){
@@ -14,24 +17,55 @@ export class Wave {
         this.centerX = stageWidth / 2;
         this.centerY = stageHeight / 2;
 
+        this.Pointgap = this.stageWidth / (this.totalPoints -1);
+
         this.init();
     }
 
     init(){
-        this.point = new Point(
-            this.centerX,
-            this.centerY
-        );
+        this.points = [];
+
+        for(let i = 0; i < this.totalPoints; i++){
+        
+            const point = new Point(
+                this.index + i,
+                this.Pointgap * i ,
+                this.centerY);
+         
+            this.points[i] = point;
+        }
     }
 
     draw(ctx){
+      
         ctx.beginPath();
-        ctx.fillStyle = '#ff0000';
+        ctx.fillStyle = this.color;
 
-        this.point.update();
+        let prevX = this.points[0].x;
+        let prevY = this.points[0].y;
 
-        ctx.arc(this.point.x, this.point.y, 30, 0, 2 * Math.PI);
+        ctx.moveTo(prevX,prevY);
+        
+        for(let i = 1; i < this.totalPoints; i++){
+           
+            if(i < this.totalPoints -1){
+                this.points[i].update();
+            }
+        
+            const cx = (prevX + this.points[i].x) /2 ;
+            const cy = (prevY + this.points[i].y) /2 ;
+
+            ctx.quadraticCurveTo(prevX, prevY,cx, cy);
+
+            prevX = this.points[i].x;
+            prevY = this.points[i].y;
+        }
+
+        ctx.lineTo(prevX,prevY);
+        ctx.lineTo(this.stageWidth,this.stageHeight);
+        ctx.lineTo(this.points[0].x, this.stageHeight);
         ctx.fill();
+        ctx.closePath();
     }
 
 }
